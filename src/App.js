2216d434f1538f6f64065565a4a460f30f2ec4dd
East <eastmaels@gmail.com>
2020-02-08 12:34:46 +0900

    add locale church show view

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import dataProvider from './dataProvider';
 import { MemberList, MemberEdit } from './components/members';
-import { LocaleChurchesList } from './components/locale_churches';
+import { LocaleChurchesList, LocaleChurchShow } from './components/locale_churches';
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="members" list={MemberList} edit={MemberEdit} />
-        <Resource name="locale_churches" list={LocaleChurchesList} perPage={25} />
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="members"
+      list={MemberList}
+      edit={MemberEdit}
+    />
+    <Resource
+      name="locale_churches"
+      list={LocaleChurchesList}
+      show={LocaleChurchShow}
+      perPage={25} />
+  </Admin>
 );
 
 export default App;
--- src/components/locale_churches.js
+++ src/components/locale_churches.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Datagrid, TextField, Show, SimpleShowLayout } from 'react-admin';
 
 export const LocaleChurchesList = props => (
-    <List { ...props } perPage={25}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-        </Datagrid>
-    </List>
+  <List {...props} perPage={25}>
+    <Datagrid rowClick="show">
+      <TextField source="id" />
+      <TextField source="name" />
+    </Datagrid>
+  </List>
 )
+
+export const LocaleChurchShow = props => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="name" />
+      <TextField source="location" />
+    </SimpleShowLayout>
+  </Show>
+);
